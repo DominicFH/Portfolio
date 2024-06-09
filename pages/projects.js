2db42c5e8dd8b0da42be65736cda12b0c3c1ecd1
DominicFH <dominicfrancisharris@gmail.com>
2024-06-09 18:36:05 +0100

    Add WiP

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Layout from "../components/Layout";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <Head>
+        <title>My Projects</title>
+      </Head>
+      <div>
+        <h2>My projects</h2>
+      </div>
+    </Layout>
+  );
+}
