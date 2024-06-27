f65a0b56982a79bd261d1683960ae8b7bed58a08
DominicFH <dominicfrancisharris@gmail.com>
2024-06-27 22:47:57 +0100

    Finish projects page

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -2,32 +2,38 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ProjectCard = ({ project: { name, link, desc, challenge, roomForImprovement } }) => {
+const ProjectCard = ({ project: { name, link, desc, challenge, roomForImprovement, skillLogo } }) => {
   console.log(link);
   return (
-    <div className="card bg-base-200 w-96 shadow-xl h-fit mx-8 grow">
-      <div className="card-body">
+    <div className="card bg-base-200 w-96 shadow-xl mx-8">
+      <div className="card-compact p-5">
         <div className="grid grid-cols-2 gap-4">
-          <h2 className="card-title w-auto text-xl">{name}</h2>
+          <h2 className="card-title w-auto text-2xl">{name}</h2>
           <div className="justify-self-end relative w-12 h-12">
-            <Image src="/images/nextjs-icon.png" fill />
+            <Image src={`/images/${skillLogo}`} fill />
           </div>
         </div>
-        <div className="my-8">
-          <h3 className="text-lg">Description</h3>
+        <div className="project-desc my-2">
+          <h3 className="text-lg">Description:</h3>
           <p>{desc}</p>
-          <h3 className="text-lg">Challenge</h3>
+        </div>
+        <div className="project-challenges my-2">
+          <h3 className="text-lg">Challenge:</h3>
           <p>{challenge}</p>
-          <h3 className="text-lg">Things to improve upon</h3>
+        </div>
+        <div className="project-improvements my-2">
+          <h3 className="text-lg">Things to improve upon:</h3>
           <p>{roomForImprovement}</p>
         </div>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">
-            <Link href={link}>View Repo</Link>
-            <div className="justify-self-end relative w-8 h-8">
-              <Image src="/images/github-icon.png" fill />
-            </div>
-          </button>
+          <Link href={link}>
+            <button className="btn btn-primary">
+              View Repo
+              <div className="justify-self-end relative w-8 h-8">
+                <Image src="/images/github-icon.png" fill />
+              </div>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- pages/projects.js
+++ pages/projects.js
@@ -12,8 +12,27 @@ export default function Projects() {
       challenge: "My first time using Next.js, this was an opportunity to learn the quirks and improvements on React",
       roomForImprovement:
         "Although I am overall happy with the styling, I will revisit the site to improve aspects of this such as the colour scheme being more readable and making the site adaptive to mobile view",
+      skillLogo: "nextjs-icon.png",
     },
-    { name: "projectTwo", link: "", desc: "Description" },
+    {
+      name: "News Site Web App",
+      link: "https://github.com/DominicFH/nc-news",
+      desc: "A web app for news articles. Built using React.js including React Router and integrating an API using Axios",
+      challenge:
+        "As my first React project, this was a great experience in using and understanding hooks and linking a front end to a back end in a seamless manner",
+      roomForImprovement: "With a tight deadline my focus was just on the MVP, there are features such as pagination that I would have liked to implement",
+      skillLogo: "reactjs-icon.png",
+    },
+    {
+      name: "News Site API",
+      link: "https://github.com/DominicFH/dominic-news-project",
+      desc: "An api providing news articles. Using PSQL, Express and tested with Jest this was an opportunity to learn to use data with JSON and understand how data is transferred across the web",
+      challenge:
+        "The main challenge was becoming comfortable with TDD however I found myself able to lean into writing tests first and not getting ahead of myself writing unsafe code",
+      roomForImprovement:
+        "I'd like to make use of my current SQL knowledge gained from work and use a wider variety of queries using aggregate functions and pivots for example to unlock more specific features for the news site",
+      skillLogo: "expressjs-icon.png",
+    },
   ];
   return (
     <Layout>
@@ -22,7 +41,7 @@ export default function Projects() {
       </Head>
       <Main>
         <div className="h-full flex justify-items-center">
-          <div className="carousel carousel-center rounded-box flex-auto items-center">
+          <div className="carousel carousel-center rounded-box flex-initial items-center">
             {projects.map((project) => {
               return <ProjectCard project={project} />;
             })}
